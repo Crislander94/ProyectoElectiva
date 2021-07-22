ebd5a90ff0ef89fb96bbc71b211eb5fdf949bd7c
Electiva-mosquez12 <87550621+Electiva-mosquez12@users.noreply.github.com>
2021-07-22 17:58:41 -0500

    update

--- app/src/main/java/com/example/agendamientoconsultorioodontologico/MainActivity2_Doctor.java
+++ app/src/main/java/com/example/agendamientoconsultorioodontologico/MainActivity2_Doctor.java
@@ -20,9 +20,13 @@ public class MainActivity2_Doctor extends AppCompatActivity {
         miTextoView1 = (TextView) findViewById(R.id.txt_descripcion);
         miTextoView2 = (TextView) findViewById(R.id.txt_descripcion2);
 
-        miTextoView1.setText("Paciente: Jose Maria Lee");
+        miTextoView1.setText("Paciente: Jose Maria Lee\n" +
+                "Fecha:15/05/2021\n" +
+                "Hora: 15:15");
         miTextoView1.setTextColor(Color.WHITE);
-        miTextoView2.setText("Paciente: Juan Macillo");
+        miTextoView2.setText("Paciente: Juan Raul Macillo\n" +
+                "Fecha: 16/05/2021\n" +
+                "Hora: 16:00");
         miTextoView2.setTextColor(Color.WHITE);
     }
 
