cca4a5dd785b6bc05f5183f1eafc86d74b15bbf5
Crislander94 <crhighlander94@gmail.com>
2021-07-21 23:48:58 -0500

    update

---
+++ app/src/main/java/com/example/agendamientoconsultorioodontologico/MainActivityPaciente.java
@@ -0,0 +1,14 @@
+package com.example.agendamientoconsultorioodontologico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityPaciente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_paciente);
+    }
+}
