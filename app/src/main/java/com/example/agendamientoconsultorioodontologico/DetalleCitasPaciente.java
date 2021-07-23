fc959dbeb9a486bdd9fc2516ed8778736a0ba55c
Crislander94 <crhighlander94@gmail.com>
2021-07-23 04:37:18 -0500

    finish diseño modulo paciente

--- app/src/main/java/com/example/agendamientoconsultorioodontologico/DetalleCitasPaciente.java
+++ app/src/main/java/com/example/agendamientoconsultorioodontologico/DetalleCitasPaciente.java
@@ -2,7 +2,9 @@ package com.example.agendamientoconsultorioodontologico;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class DetalleCitasPaciente extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class DetalleCitasPaciente extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_citas_paciente);
+    }
+    public void GoListCitasPaciente(View view){
+        Intent i = new Intent(this, ListCitasPaciente.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/agendamientoconsultorioodontologico/ListCitasPaciente.java
+++ app/src/main/java/com/example/agendamientoconsultorioodontologico/ListCitasPaciente.java
@@ -2,7 +2,9 @@ package com.example.agendamientoconsultorioodontologico;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ListCitasPaciente extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class ListCitasPaciente extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_citas_paciente);
+    }
+
+    public void GoMainActivityMainPaciente(View view){
+        Intent i = new Intent(this, MainActivityPaciente.class);
+        startActivity(i);
+    }
+    public void GoDetalleLista(View view){
+        Intent i = new Intent(this, DetalleCitasPaciente.class);
+        startActivity(i);
     }
 }
