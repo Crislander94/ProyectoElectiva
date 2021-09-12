2d87cb6d832f061dc812f1ef8bdd7ba96a56226c
Crislander94 <crhighlander94@gmail.com>
2021-09-12 18:50:01 -0500

    update

--- app/src/main/java/com/example/agendamientoconsultorioodontologico/AgendamientosDB.java
+++ app/src/main/java/com/example/agendamientoconsultorioodontologico/AgendamientosDB.java
@@ -1,4 +1,21 @@
 package com.example.agendamientoconsultorioodontologico;
 
 public class AgendamientosDB {
+
+
+
+    interface ColumnasPaciente{
+        String ID = "id";
+        String NOMBRES = "nombres";
+        String PASSWORD = "password";
+        String CORREO = "correo";
+        String TELEFONO = "telefono";
+        String CEDULA = "cedula";
+    }
+    interface ColumnasCita{
+        String ID = "cod_cita";
+        String DOCTOR = "cod_doctor";
+        String PACIENTE = "cod_paciente";
+        String 
+    }
 }
